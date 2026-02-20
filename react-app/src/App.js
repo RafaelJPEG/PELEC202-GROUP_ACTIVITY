e626116c7ea66dd14421cf56c967081cd552dec7
Rochelle-DR <deroblescamille1705@gmail.com>
2026-02-20 23:21:33 +0800

    initial

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,8 +1,25 @@
-import Counter from './Counter.jsx'
+import React, {useState} from 'react'
 
-function App(){
+function App() {
+    
+    const [number, setNumber] = useState(0)
+    
+    const increment = () => {
+        setNumber(number + 1)
+    }
 
-    return(<Counter/>);
+    const decrement= () => {
+        setNumber(number -1)
+    }
+
+    return ( 
+        <div className="App">
+
+                <h1>{number}</h1>
+                <button  onClick={increment}>Add</button>
+        <button  onClick={decrement}>Sub</button>
+        </div>
+    );
 }
 
 export default App;
